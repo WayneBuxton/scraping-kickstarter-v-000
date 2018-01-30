1104dc0f1819926c72a5e3030f100caadd857891
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-30 18:20:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,9 @@ kickstarter = Nokogiri::HTML(html)
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location: project.css("li a span.location-name").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
 
 def create_project_hash
   # write your code here
